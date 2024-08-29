92572679977a35d88e7d6a755911fbcac728b9f9
krola1 <kwl082024@gmail.com>
2024-08-29 08:15:12 +0200

    added comments and 'enter key' detections

--- main.js
+++ main.js
@@ -5,18 +5,23 @@ const inputField = document.getElementById("input-field");
 const listHeader = document.getElementById("list-header");
 //defines main array
 let toDoList = [];
-// let counter = 0; //time will tell if i can do without this
-// functions
+
+// *******functions*********
+
+//clears HTML - input-Field after use
 const clearInputField = () => {
   inputField.value = "";
 };
+//clears created HTML nodes before they are to be re-generated
 const clearHTML = () => {
   listHeader.innerHTML = "";
 };
+//transfers string data from HTML input-field to the toDoList array.
 const fromFieldToArray = () => {
   fieldValue = inputField.value;
   toDoList.push(fieldValue);
 };
+// converts data from the array to HTML nodes. Adds an ID attribute to nodes so they can be removed from array later.
 const fromArrayToHTML = () => {
   counter = 0;
   for (let item in toDoList) {
@@ -29,6 +34,9 @@ const fromArrayToHTML = () => {
   }
   counter = 0;
 };
+//Listens for clicks on HTML node "add-button".
+//When button is clicked, checks if there is data in input-field, and if true:.
+//Calls clearHTML, fromFieldToArray, fromArrayToHTML and clearInputField functions
 addButton.addEventListener("click", () => {
   if (inputField.value !== "") {
     clearHTML();
@@ -37,13 +45,29 @@ addButton.addEventListener("click", () => {
     clearInputField();
   }
 });
-
+//Listens for keystrokes in the input field, if enter-key is pressed, checks if there is data in input-field. If both are true:
+//Calls clearHTML, fromFieldToArray, fromArrayToHTML and clearInputField functions
+inputField.addEventListener("keydown", (key) => {
+  if (key.key === "Enter") {
+    if (inputField.value !== "") {
+      clearHTML();
+      fromFieldToArray();
+      fromArrayToHTML();
+      clearInputField();
+    }
+  }
+});
+// Listens for clicks on HTML node clear-button.
+//When the clear-button is pressed, sets the value of toDoList to an empty array and calls clearInputField and clear HTML functions.
 clearButton.addEventListener("click", () => {
   toDoList = [];
   clearInputField();
   clearHTML();
 });
 
+//Listens for clicks on the entire HTML document, returns the .target value for each element clicked.
+//When node with target that matches for <li> elements is clicked, splice toDoList. Uses ID attribute to define splicepoint.
+//also calls clearHTML and formArrayToHTML functions
 document.addEventListener("click", (removeByClick) => {
   target = removeByClick.target;
   if (target.toString() === "[object HTMLLIElement]") {
